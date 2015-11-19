a6c249729fa559dfb3451ac6895944d84a321ab1
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2015-11-19 20:21:36 +0530

    Update fingerprints
    Signed-off-by: Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>

--- config/common_fingerprints.mk
+++ config/common_fingerprints.mk
@@ -2,22 +2,22 @@ ifeq ($(TARGET_PRODUCT),minimal_hammerhead)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58K/2256973:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58K 2256973 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58N/2324235:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58N 2324235 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),minimal_mako)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/mako/mako:6.0/MRA58K/2256973:user/release-keys \
-    PRIVATE_BUILD_DESC="mako-user 6.0 MRA58K 2256973 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:6.0/MRA58V/2324235:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 6.0 MRA58V 2324235 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),minimal_shamu)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58R/2324235:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58R 2324235 release-keys"
 endif
