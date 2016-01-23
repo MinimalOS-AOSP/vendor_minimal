krexus/vendor_krexus

Bootanimation: Better choosing logic
 * looks better and creates a more usable directory tree

products/common.mk
@@ -21,9 +21,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGE_OVERLAYS += vendor/krexus/overlay/common
 
 # Bootanimation (add if it exists)
-ifneq ($(wildcard vendor/krexus/prebuilt/$(TARGET_DEVICE)/bootanimation.zip),)
+ifneq ($(wildcard vendor/krexus/prebuilt/bootanimation/$(TARGET_DEVICE).zip),)
 PRODUCT_COPY_FILES += \
-        vendor/krexus/prebuilt/$(TARGET_DEVICE)/bootanimation.zip:system/media/bootanimation.zip
+        vendor/krexus/prebuilt/bootanimation/$(TARGET_DEVICE).zip:system/media/bootanimation.zip
 endif
 
 # Extra Packages

MinimalOS-AOSP/vendor_minimal

Conflicts:
	prebuilt/bootanimation/bullhead.zip
	prebuilt/bootanimation/grouper.zip
	prebuilt/bootanimation/hammerhead.zip
	prebuilt/bootanimation/mako.zip
	prebuilt/bootanimation/shamu.zip
	products/common.mk

config/common.mk
@@ -71,6 +71,8 @@ PRODUCT_COPY_FILES += \
     vendor/minimal/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/minimal/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 	
-# Bootanimation
- PRODUCT_COPY_FILES += \
-         vendor/minimal/prebuilt/$(TARGET_DEVICE)/bootanimation.zip:system/media/bootanimation.zip	
+# Bootanimation (add if it exists)
+ifneq ($(wildcard vendor/minimal/prebuilt/bootanimation/$(TARGET_DEVICE).zip),)
+PRODUCT_COPY_FILES += \
+        vendor/vendor/prebuilt/bootanimation/$(TARGET_DEVICE).zip:system/media/bootanimation.zip
+endif