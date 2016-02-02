5b1a9cb7adceea5c32c21fb5e12fb26fdf4ea528
Aldrin Holmes (CallMeAldy) <hello@aldrinholmes.com>
2016-02-02 13:45:41 +0100

    Update fingerprints
    Signed-off-by: Aldrin Holmes (CallMeAldy) <hello@aldrinholmes.com>

--- config/common_fingerprints.mk
+++ config/common_fingerprints.mk
@@ -2,30 +2,30 @@ ifeq ($(TARGET_PRODUCT),minimal_angler)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29Q 2480792 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),minimal_hammerhead)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29S/2489379:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29S 2489379 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29Q 2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29Q 2480792 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),minimal_mako)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:6.0.1/MMB29S/2489379:user/release-keys \
-    PRIVATE_BUILD_DESC="mako-user 6.0.1 MMB29S 2489379 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="mako-user 6.0.1 MMB29Q 2480792 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),minimal_shamu)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MMB29S/2489379:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29S 2489379 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29Q 2480792 release-keys"
 endif
