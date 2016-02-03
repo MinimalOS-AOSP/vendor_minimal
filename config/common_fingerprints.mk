3216ceeedcf9fcf708af536673bc3dc4346edc30
Aldrin Holmes <hello@aldrinholmes.com>
2016-02-03 22:00:27 +0530

    Such derp. Much wow

--- config/common_fingerprints.mk
+++ config/common_fingerprints.mk
@@ -10,7 +10,7 @@ ifeq ($(TARGET_PRODUCT),minimal_hammerhead)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29Q 2480792:user/release-keys \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29Q/2480792:user/release-keys \
     PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29Q 2480792 release-keys"
 endif
 
